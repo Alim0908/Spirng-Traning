617f647d32461d3197d27d76d5c178584afca929
Alim <laysuinfo@gmail.com>
2024-03-22 18:51:49 +0300

    CommentRepository

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+
+public class CommentService {
+    public void  publishComment(Comment comment){
+        // save in the database
+        //sen email
+    }
+}
