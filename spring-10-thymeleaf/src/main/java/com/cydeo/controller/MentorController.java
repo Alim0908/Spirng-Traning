2f373ce1720a023a1a9f201231754a495c636bc9
Alim <laysuinfo@gmail.com>
2024-04-05 15:33:57 +0300

    Auto wiring

--- spring-10-thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,16 +1,34 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
-    @GetMapping("/register") //localhost:8080/mentor/register
-    public String register(){
 
-        return "mentor/register";
+    @GetMapping("/register") //localhost:8080/mentor/register
+    public String register(Model model) {
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList", batchList);
+
+        model.addAttribute("mentor", new Mentor());
+//        after we register all the information , this attrubuteName:"mentor" will store all the information we puted like, firstname , lastname...
+        return "mentor/mentor-register";
+    }
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+        // how can i access  these mentor attributes dwe input in the browser
+        //by putting @ModelAttribute("mentor") Mentor mentor to the parameter
+        return "mentor/mentor-confirmation";
     }
 
 
