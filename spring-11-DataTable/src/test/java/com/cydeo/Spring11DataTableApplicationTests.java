2dac70c0dcf911d2aea9f501effc2666889f794b
Alim <laysuinfo@gmail.com>
2024-05-04 01:37:18 +0300

    connecting my application to database

--- spring-11-DataTable/src/test/java/com/cydeo/Spring11DataTableApplicationTests.java
+++ spring-11-DataTable/src/test/java/com/cydeo/Spring11DataTableApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring11DAtaTableCreationApplicationTests {
+class Spring11DataTableApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
