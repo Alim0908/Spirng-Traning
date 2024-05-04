2901c1880d27dacf981fb48d3c016859c6ef4dcc
Alim <laysuinfo@gmail.com>
2024-05-04 16:52:31 +0300

    car entity

--- spring-11-DataTable/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-DataTable/src/main/java/com/cydeo/entity/Account.java
@@ -1,13 +1,13 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-@Entity
+@MappedSuperclass// means this class represents this class only providing inheritance relationship to other entities
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
     private String owner;
     private BigDecimal balance;
