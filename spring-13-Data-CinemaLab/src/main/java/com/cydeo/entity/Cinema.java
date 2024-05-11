d3ea1ee553e7d2aa22191940595e5be665341102
Alim <laysuinfo@gmail.com>
2024-05-11 23:20:59 +0300

    location

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -0,0 +1,14 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class Cinema  extends BaseEntity{
+   private String name;
+  private String sponsoredName;
+@ManyToOne(fetch = FetchType.LAZY)
+  private Location location;
+}
