aa3a44fa1b223d2ead8e790741ffad54e574ee9a
Alim <laysuinfo@gmail.com>
2024-05-14 16:30:15 +0300

    spring data queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -9,14 +9,17 @@ import java.util.List;
 public interface RegionRepository extends JpaRepository<Region, Integer> {
     //display all the regions in canada
     List<Region> findByCountry(String country);
+
     List<Region> getByCountry(String country);
-//display all regions with country name includes 'United'
-List<Region>  findByCountryContaining(String country);
+
+    //display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
     //Display all regions with country name includes 'United' in order(region)
-List<Region> findByCountryContainsOrderByRegionDesc(String country);
+    List<Region> findByCountryContainsOrderByRegionDesc(String country);
 
-
-
-
+//Display top 2 regions in United States
+List<Region> findTopByCountry(String country);
+List<Region> findTop2ByCountry(String country);
 
 }
