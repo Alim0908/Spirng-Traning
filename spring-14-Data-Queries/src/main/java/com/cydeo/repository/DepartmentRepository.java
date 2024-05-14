a2059e612c3f98e6f8c575cda730a29b4cd8a84b
Alim <laysuinfo@gmail.com>
2024-05-14 20:01:53 +0300

    spring data derived queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+
     //Display all departments in the Furniture Department
     List<Department> findByDepartment(String department);
 
