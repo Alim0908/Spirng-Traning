1d36ff5937188c5a26eff96e5ca31e002facaa1e
Alim <laysuinfo@gmail.com>
2024-05-19 22:11:25 +0300

    spring data queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
@@ -4,6 +4,10 @@ import com.cydeo.entity.MovieCinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
 
@@ -11,24 +15,26 @@ public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> 
 
     //Write a derived query to read movie cinema with id
 
+Optional<MovieCinema> findById(Long id);
+
 
     //Write a derived query to count all movie cinemas with a specific cinema id
-
+Integer countAllByCinemaId(Long id);
 
     //Write a derived query to count all movie cinemas with a specific movie id
-
+    Integer countAllByMovieId(Long id);
 
     //Write a derived query to list all movie cinemas with higher than a specific date
-
+    List<MovieCinema> findAllByDateTimeAfter(LocalDateTime dateTime);
 
     //Write a derived query to find the top 3 expensive movies
-
+    List<MovieCinema> find3TopByOrderByMoviePriceDesc();
 
     //Write a derived query to list all movie cinemas that contain a specific movie name
-
+    List<MovieCinema> findAllByMovie_NameContaining(String name);
 
     //Write a derived query to list all movie cinemas in a specific location name
-
+    List<MovieCinema> findAllByCinema_Location_Name(String name);
 
     // ------------------- JPQL QUERIES ------------------- //
 
