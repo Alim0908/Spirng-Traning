bb8e11b83d3c13f5672b7434151ce63cbd39d86a
Alim <laysuinfo@gmail.com>
2024-07-01 21:27:42 +0300

    spring REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -22,8 +22,30 @@ public class CourseController {
     }
 
     @GetMapping("{id}")
-    public CourseDTO getCourseById(@PathVariable("id") long courseId){
+    public CourseDTO getCourseById(@PathVariable("id") long courseId) {
 
         return courseService.getCourseById(courseId);
     }
+
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category) {
+        return courseService.getCoursesByCategory(category);
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course) {
+
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") long courseId, @RequestBody CourseDTO course) {
+        courseService.updateCourse(courseId, course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") long courseId) {
+
+    }
+
 }
