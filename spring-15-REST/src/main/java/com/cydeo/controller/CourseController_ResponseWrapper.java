0d3f967355e5649ccdc22bc7bb4bd6ccd9aee56f
Alim <laysuinfo@gmail.com>
2024-07-03 11:59:23 +0300

    spring REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -12,27 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/courses/api/v3")
 public class CourseController_ResponseWrapper {
-
-    private final CourseService courseService;
+private final CourseService courseService;
 
     public CourseController_ResponseWrapper(CourseService courseService) {
         this.courseService = courseService;
     }
 
-    @GetMapping
-    public ResponseEntity<ResponseWrapper> getAllCourses(){
-
-        return ResponseEntity
+    public ResponseEntity<ResponseWrapper> getAllCourse(){
+       return  ResponseEntity
                 .status(HttpStatus.ACCEPTED)
-                .header("Version","Cydeo.V3")
+                .header("Version","Cydeo.v3")
                 .body(new ResponseWrapper("courses successfully retrieved",courseService.getCourses()));
     }
-
-    @GetMapping("{id}")
+@GetMapping("{id}")
     public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId){
-        return ResponseEntity.ok(new ResponseWrapper("course:" + courseId + "retrieved",courseService.getCourseById(courseId)));
-
+        return ResponseEntity.ok(new ResponseWrapper("course:"+courseId+"retrieved",courseService.getCourseById(courseId)));
     }
+
+
 }
 
 
