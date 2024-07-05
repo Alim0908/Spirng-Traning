ed91dfc52da453b0c1a66df41477856d740aab0a
Alim <laysuinfo@gmail.com>
2024-07-05 12:39:06 +0300

    Genre Service

--- spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/service/GenreService.java
+++ spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/service/GenreService.java
@@ -1,2 +1,8 @@
-package com.cydeo.service;public interface GenreSerrvice {
+package com.cydeo.service;
+
+import com.cydeo.dto.GenreDTO;
+
+public interface GenreService {
+    GenreDTO save(GenreDTO genreDTO);
+    void deleteById(Long id);
 }
